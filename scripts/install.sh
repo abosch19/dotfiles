5e0455026ad0fbee9c35be74e1efac3514233726
Alex Bosch Grabulos <alexboschgrabulos@MacBook-Pro-de-Alex-Bosch.local>
2022-05-31 16:19:58 +0200

    feat: git configuration.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,11 @@
+# Ask for the administrator password upfront
+sudo -v 
+
 echo "# Custom shell aliases and functions"  >> "$HOME_PATH/.zshrc"
 echo "source $DOTFILES_PATH/shell/init.sh"  >> "$HOME_PATH/.zshrc"
+
+echo "🔀 Setting Git config"
+read -rp "✉️ What is you email? " git_email
+read -rp "👤 And your name? " git_name
+git config --global user.email "$git_email"
+git config --global user.name "$git_name"
