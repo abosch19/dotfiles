3e7096f2a2445fd684c13ab0162cf766a4c32461
Alex Bosch <abosch19@gmail.com>
2023-04-03 15:54:54 +0800

    fix: killport function working again.

--- shell/_functions/utils.sh
+++ shell/_functions/utils.sh
@@ -3,5 +3,19 @@ ip() {
 }
 
 killport() {
- kill -6 $(lsof -t -i:$1 | awk '{print $2}')
+  if [ -z "$1" ]; then
+        echo "Usage: killport <port>"
+        return 1
+    fi
+
+    port=$1
+    pid=$(lsof -t -i:$port)
+
+    if [ -z "$pid" ]; then
+        echo "No process running on port $port"
+        return 1
+    fi
+
+    echo "Killing process $pid on port $port"
+    kill -9 "$pid"
 }
