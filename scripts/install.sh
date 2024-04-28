19347071666d3add14885fb6ebdfaabdc55a3796
Alex Bosch <abosch19@gmail.com>
2024-04-28 13:16:14 +0300

    feat: source zshrc file on finish

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,11 @@ echo "" >> $ZSHRC_PATH
 echo "# Custom shell aliases and functions"  >> $ZSHRC_PATH
 echo "source $DOTFILES_PATH/shell/init.sh"  >> $ZSHRC_PATH
 
+source $ZSHRC_PATH
+
 echo "🔀 Setting Git config"
 read -rp "✉️ What is you email? " git_email
 read -rp "👤 And your name? " git_name
 git config --global user.email "$git_email"
 git config --global user.name "$git_name"
+
